ae2a595dbaa1f8392ff9032729a691c5a842c734
Stratus3D <admin@stratus3d.com>
2017-12-29 14:52:07 -0500

    Correct mistakes in utils.sh.

--- bin/utils.sh
+++ bin/utils.sh
@@ -8,13 +8,12 @@ download_kerl() {
     echo "Downloading kerl..."
     local kerl_version="1.8.1"
     local kerl_url="https://raw.githubusercontent.com/kerl/kerl/${kerl_version}/kerl"
-    curl -Lo $kerl_url "$(kerl_path)"
+    curl -Lso $kerl_url "$(kerl_path)"
     chmod +x "$(kerl_path)"
 }
 
 kerl_path() {
-    # TODO: This may be wrong
-    "kerl"
+    echo "kerl"
 }
 
 update_available_versions() {
