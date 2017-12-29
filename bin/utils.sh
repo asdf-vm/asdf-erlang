06f5d8572a80b4689b85923ca0ff57351edf58eb
Stratus3D <admin@stratus3d.com>
2017-12-29 15:06:23 -0500

    Correct a few more issues in utils.sh.

--- bin/utils.sh
+++ bin/utils.sh
@@ -5,17 +5,20 @@ ensure_kerl_installed() {
 }
 
 download_kerl() {
-    echo "Downloading kerl..."
+    # Print to stderr so asdf doesn't assume this string is a list of versions
+    echo "Downloading kerl..." >&2
+
     local kerl_version="1.8.1"
     local kerl_url="https://raw.githubusercontent.com/kerl/kerl/${kerl_version}/kerl"
-    curl -Lso $kerl_url "$(kerl_path)"
+
+    curl -Lso "$(kerl_path)" $kerl_url
     chmod +x "$(kerl_path)"
 }
 
 kerl_path() {
-    echo "kerl"
+    echo "$(dirname "$(dirname $0)")/kerl"
 }
 
 update_available_versions() {
-    "$(kerl_path)" update releases
+   "$(kerl_path)" update releases > /dev/null
 }
