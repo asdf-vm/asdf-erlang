fe7fa02cf4e2dea9ddd3d0dadb34c54b0a0e1318
Trevor Brown <Stratus3D@users.noreply.github.com>
2021-08-06 09:07:31 -0400

    Improve output for the install command (#185)
    * Improve output for the install command
    * Correct conditional for install failure message
    * Properly handle non-zero exit codes in ensure_kerl_setup
    * Add bash strict mode for better output
    
    Fixes #182

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,9 +1,21 @@
 KERL_VERSION="2.1.2"
 
+handle_failure() {
+  function=$1
+  error_message=$2
+  $function && exit_code=$? || exit_code=$?
+
+  if [ "$exit_code" -ne 0 ]; then
+    printf "$error_message\\n" 1>&2
+  fi
+
+  return "$exit_code"
+}
+
 ensure_kerl_setup() {
-  set_kerl_env
-  ensure_kerl_installed
-  update_available_versions
+  handle_failure set_kerl_env 'Failed to set kerl environment'
+  handle_failure ensure_kerl_installed 'Failed to install kerl'
+  handle_failure update_available_versions 'Failed to update available versions'
 }
 
 ensure_kerl_installed() {
